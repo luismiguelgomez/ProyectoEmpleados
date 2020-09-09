9d06867057306d445de5839dd3876239999ce45c
luismiguelgomez <gomezpaezluismiguel@gmail.com>
2020-09-08 21:25:15 -0500

    Primera version

---
+++ src/unbosque/edu/co/AppMain.java
@@ -0,0 +1,13 @@
+package unbosque.edu.co;
+
+public class AppMain {
+
+	public AppMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		//Esta es la clase encargada de comenzar a ejecutar el codigo
+	}
+
+}
