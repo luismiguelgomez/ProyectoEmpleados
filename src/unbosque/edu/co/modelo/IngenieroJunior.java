c60c7fd2392549cfe5cae3341f0f68392aee49db
luismiguelgomez <gomezpaezluismiguel@gmail.com>
2020-09-17 18:34:53 -0500

    Creando datos de ing junior,senior y comision

--- src/unbosque/edu/co/modelo/IngenieroJunior.java
+++ src/unbosque/edu/co/modelo/IngenieroJunior.java
@@ -1,51 +1,50 @@
-/**
- * En esta clase se pretende hacer las operacion para calcular el salario del
- * Ingeniero Junior por nivel que ha alcanzado y el porcentaje definido
- */
-package unbosque.edu.co.modelo;
-/**
- * @author rodas
- *
- */
-public class IngenieroJunior implements EmpleadoFijo{
-	/**
-	 * 
-	 */
-	private boolean nivel1;
-	private boolean nivel2;
-	private boolean nivel3;
-	private boolean nivel4;
-	private boolean nivel5;
-
-	public IngenieroJunior() {
-	
-	int nivel1 = salarioBase;
-
-	}
-	@Override
-	/**
-	 * <pre>
-	 * 1. Tener implementada una interfaz (InterfaceEmpleadoFijo) que tenga el metodo porcentaje adicional<br>
-	 * 2. Desde la clase en la cual se va a crear el objeto es necesario enviar los parametros de anios<br>
-	 * </pre>
-	 * @param int anios , para hacer calculo de porcentaje Adicional
-	 */
-	public void porcentajeAdicional(int anios) {
-	}
-
-/**
- * el metodo de nivel dos y tres (nivelDosTres) se le incrementa un 5% al salario base
- */
-	public void nivelDosTres(){
-	int nivel2 = (salarioBase*0.05)+salarioBase;
-	int nivel3 = (salarioBase*0.05)+salarioBase;
-	}
-/**
- * el metodo de nivel dos y tres (nivelDosTres) se le incrementa un 8% al salario base
- */
-	public void nivelCuatroCinco(){
-	int nivel4 = (salarioBase*0.08)+salarioBase;
-	int nivel5 = (salarioBase*0.08)+salarioBase;
-	}
-}
-
+///**
+// * En esta clase se pretende hacer las operacion para calcular el salario del
+// * Ingeniero Junior por nivel que ha alcanzado y el porcentaje definido
+// */
+//package unbosque.edu.co.modelo;
+///**
+// * @author rodas
+// *
+// */
+//public class IngenieroJunior implements EmpleadoFijo{
+//	/**
+//	 * 
+//	 */
+//	private boolean nivel1;
+//	private boolean nivel2;
+//	private boolean nivel3;
+//	private boolean nivel4;
+//	private boolean nivel5;
+//
+//	public IngenieroJunior() {
+//	
+//	int nivel1 = salarioBase;
+//
+//	}
+//	@Override
+//	/**
+//	 * <pre>
+//	 * 1. Tener implementada una interfaz (InterfaceEmpleadoFijo) que tenga el metodo porcentaje adicional<br>
+//	 * 2. Desde la clase en la cual se va a crear el objeto es necesario enviar los parametros de anios<br>
+//	 * </pre>
+//	 * @param int anios , para hacer calculo de porcentaje Adicional
+//	 */
+//	public void porcentajeAdicional(int anios) {
+//	}
+//
+///**
+// * el metodo de nivel dos y tres (nivelDosTres) se le incrementa un 5% al salario base
+// */
+//	public void nivelDosTres(){
+//	int nivel2 = (salarioBase*0.05)+salarioBase;
+//	int nivel3 = (salarioBase*0.05)+salarioBase;
+//	}
+///**
+// * el metodo de nivel dos y tres (nivelDosTres) se le incrementa un 8% al salario base
+// */
+//	public void nivelCuatroCinco(){
+//	int nivel4 = (salarioBase*0.08)+salarioBase;
+//	int nivel5 = (salarioBase*0.08)+salarioBase;
+//	}
+//}
